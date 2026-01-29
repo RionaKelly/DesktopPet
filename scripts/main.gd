55d5cfe719f912c4b9adf736898db84db90aab5f
Riona <c00296706@setu.ie>
2026-01-29 16:56:44 +0000

    scaling based on resolution started

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,13 +1,33 @@
-#Followed tutorial by Godot Dev Checkpoint to learn about making the Windows function for this game
+# Followed tutorial by Godot Dev Checkpoint to learn about windows with Godot
+# Used this for window presets (invisible, borderless etc.) and making them move around the screen on their own
+# https://www.youtube.com/watch?v=9JHFrnt5j_k
+
 extends Node2D
 
-var move_speed = 5
-var direction = Vector2(1, 0) # Moving Right
-var decision_time: bool = true
+# 'The Safe Area'
+# screen_get_usable_rect() returns screen area minus taskbar/docks
+var usable_rect = DisplayServer.screen_get_usable_rect()
+
+var move_speed = usable_rect.end.x * 0.001 # Pet speed based on the size of the screen
+var direction = Vector2(0, 0) # Not Moving
+var decision_time: bool = false # Pet must wait Timer wait time before making their first decision
 
 func _ready() -> void:
 	#get access to the OS Window (not just the game node)
 	var window = get_window()
+	
+	# Prints for bugtesting
+	print(usable_rect.end)
+	print(usable_rect.position)
+	print(usable_rect.size)
+	
+	# Calculates pet (window and sprite) size based on monitor size
+	var pet_size = (usable_rect.end.x * usable_rect.end.y) / 29536 # (3840*2064)/160=49536 (1920*1080)/160=12960
+	var pet_scale = ((pet_size/160) * 1)
+	print(pet_size)
+	print(pet_scale)
+	window.size = Vector2i(pet_size, pet_size)
+	$AnimatedSprite2D.scale = Vector2(pet_scale, pet_scale)
 	
 	# We enable transparency for both the Godot Viewport and OS Window
 	get_viewport().transparent_bg = true
@@ -19,26 +39,21 @@ func _ready() -> void:
 	# Keep them above everything
 	window.always_on_top = true
 	
-	# Force Windows to let us be borderless
+	# Force borderless
 	window.unresizable = false
 	
 	# Find the floor
-	# 1. Get the safe area of screen
-	var usable_rect = DisplayServer.screen_get_usable_rect()
-	# 2. Calculate the floor position
-	# end.y is pixel coordinate where the taskbar starts
+	# Calculate the floor position --- end.y is pixel coordinate where the taskbar starts
 	var target_y = usable_rect.end.y - window.size.y
-	# 3. Move the sprite there
-	# x = 0 (left edge), y = target_y (the floor)
-	window.position = Vector2i(0, target_y)
+	# Move the sprite there --- x = center of screen, y = the floor
+	window.position = Vector2i((usable_rect.end.x / 2) - (window.size.x / 2), target_y)
+	
+	# Start the timer for pet decision
+	$Timer.start()
 	
 func _process(_delta):
 	#get access to the OS Window (not just the game node)
 	var window = get_window()
-	
-	# The Safe Area
-	# screen_get_usable_rect() returns screen area minus taskbar/docks
-	var usable_rect = DisplayServer.screen_get_usable_rect()
 	
 	# Vector2i used to tell Windows to move to an exact pixel coordinate (integer) 
 	# Calculate the move
@@ -85,8 +100,10 @@ func _process(_delta):
 		direction.x = direction.x * -1 # Change Direction
 		if $AnimatedSprite2D.flip_h == true:
 			$AnimatedSprite2D.flip_h = false
+			print("Bounce off left")
 		else:
 			$AnimatedSprite2D.flip_h = true
+			print("Bounce off right")
 
 # Resets the decision timer after random amount of seconds
 func _on_timer_timeout():
