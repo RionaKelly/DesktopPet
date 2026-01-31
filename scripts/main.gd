6d15abd4f88f9a4d2b84ce4013b3c852c9481a8b
Riona <c00296706@setu.ie>
2026-01-31 10:17:55 +0000

    scaling works better now

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,31 +1,40 @@
 # Followed tutorial by Godot Dev Checkpoint to learn about windows with Godot
-# Used this for window presets (invisible, borderless etc.) and making them move around the screen on their own
+# Used this for window presets (invisible, borderless etc.) and making them move around the screen
 # https://www.youtube.com/watch?v=9JHFrnt5j_k
 
 extends Node2D
 
-# 'The Safe Area'
-# screen_get_usable_rect() returns screen area minus taskbar/docks
+# Set which Screen the game should use
+# var screen_choice = 1 # 0 = primary, 1 = secondary
+
+#get access to the OS Window (not just the game node)
+@onready var window = get_window()
+
+# The 'Safe Area' that the window shouldn't leave --- usable_rect() = screen area minus taskbar/docks
 var usable_rect = DisplayServer.screen_get_usable_rect()
 
-var move_speed = usable_rect.end.x * 0.001 # Pet speed based on the size of the screen
+# Game Stats
+var move_speed = usable_rect.size.x * 0.001 # Pet speed based on the size of the screen
 var direction = Vector2(0, 0) # Not Moving
 var decision_time: bool = false # Pet must wait Timer wait time before making their first decision
 
+# Bug-Testing
+var debugMovement = false
+
 func _ready() -> void:
-	#get access to the OS Window (not just the game node)
-	var window = get_window()
+	# DisplayServer.window_set_current_screen.call_deferred(screen_choice)
+	print("Screen: ", DisplayServer.window_get_current_screen())
 	
 	# Prints for bugtesting
-	print(usable_rect.end)
-	print(usable_rect.position)
-	print(usable_rect.size)
+	print("Screen End: ", usable_rect.end)
+	print("Screen Position: ", usable_rect.position)
+	print("Screen Size: ", usable_rect.size)
 	
 	# Calculates pet (window and sprite) size based on monitor size
-	var pet_size = (usable_rect.end.x * usable_rect.end.y) / 29536 # (3840*2064)/160=49536 (1920*1080)/160=12960
-	var pet_scale = ((pet_size/160) * 1)
-	print(pet_size)
-	print(pet_scale)
+	var pet_size = (usable_rect.size.x * usable_rect.size.y) * 0.00002
+	var pet_scale = (pet_size/160.0)
+	print("Pet Size: ", pet_size)
+	print("Pet Scale ", pet_scale)
 	window.size = Vector2i(pet_size, pet_size)
 	$AnimatedSprite2D.scale = Vector2(pet_scale, pet_scale)
 	
@@ -44,17 +53,14 @@ func _ready() -> void:
 	
 	# Find the floor
 	# Calculate the floor position --- end.y is pixel coordinate where the taskbar starts
-	var target_y = usable_rect.end.y - window.size.y
+	var target_y = usable_rect.size.y - window.size.y
 	# Move the sprite there --- x = center of screen, y = the floor
-	window.position = Vector2i((usable_rect.end.x / 2) - (window.size.x / 2), target_y)
+	window.position = Vector2i((usable_rect.size.x / 2) - (window.size.x / 2), target_y)
 	
 	# Start the timer for pet decision
 	$Timer.start()
 	
 func _process(_delta):
-	#get access to the OS Window (not just the game node)
-	var window = get_window()
-	
 	# Vector2i used to tell Windows to move to an exact pixel coordinate (integer) 
 	# Calculate the move
 	var move_vector = Vector2i(direction * move_speed)
@@ -67,19 +73,24 @@ func _process(_delta):
 	if decision_time == true:
 		if rand_choice < 0.4 and direction.x != 0:
 			direction.x = 0
-			print("Stop")
-		elif rand_choice < 0.65 and window.position.x + window.size.x < ((usable_rect.end.x)*0.9):
+			if debugMovement:
+				print("Stop")
+		elif rand_choice < 0.65 and window.position.x + window.size.x < ((usable_rect.size.x)*0.9):
 			direction.x = 1
 			decision_time = false
-			print("Turn Right")
-		elif rand_choice < 0.9 and window.position.x > (usable_rect.end.x * 0.1):
+			if debugMovement:
+				print("Turn Right")
+		elif rand_choice < 0.9 and window.position.x > (usable_rect.size.x * 0.1):
 			direction.x = -1
-			print("Turn Left")
+			if debugMovement:
+				print("Turn Left")
 		else:
-			print("No Change")
+			if debugMovement:
+				print("No Change")
 		decision_time = false
 		$Timer.wait_time = rand_wait
-		print(rand_wait)
+		if debugMovement:
+			print("Wait ", rand_wait)
 		$Timer.start()
 		
 	# Makes pet face the right direction and play the right animation after moving/stopping
@@ -96,16 +107,23 @@ func _process(_delta):
 	window.position += move_vector
 	
 	# Check edges to flip in case touching
-	if window.position.x + window.size.x > usable_rect.end.x or window.position.x < usable_rect.position.x:
+	if window.position.x + window.size.x > usable_rect.size.x or window.position.x < usable_rect.position.x:
 		direction.x = direction.x * -1 # Change Direction
 		if $AnimatedSprite2D.flip_h == true:
 			$AnimatedSprite2D.flip_h = false
-			print("Bounce off left")
+			if debugMovement:
+				print("Bounce off left")
 		else:
 			$AnimatedSprite2D.flip_h = true
-			print("Bounce off right")
+			if debugMovement:
+				print("Bounce off right")
 
 # Resets the decision timer after random amount of seconds
 func _on_timer_timeout():
 	decision_time = true
-	print("Decide")
+	if debugMovement:
+		print("Decide")
+
+# Waits for click to change sprite
+# func change_sprite(event):
+	
